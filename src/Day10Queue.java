96175efc7c1406c425abd3e92ac929a42f77bd1c
Gokul <gokulr25ct@vetias.ac.in>
2026-06-16 15:21:59 +0530

    day10Queue.java

--- src/Day10Queue.java
+++ src/Day10Queue.java
@@ -1,17 +1,23 @@
+import java.util.LinkedList;
 public class Day10Queue {
+    static void process(LinkedList<String> list) {
+     String remove = list.removeFirst();
+     System.out.println("Remove"+remove);
+
+    }
     static void main() {
-        Linkedlist<string> list = new LinkedList();
-        Linkedlist.add("Arun");
-        Linkedlist.add("Guna");
-        Linkedlist.add("Sanjay");
-        Linkedlist.add("saran prasath");
-        Linkedlist.add("Dharanish");
+        LinkedList <String> list = new LinkedList<String>();
+        list.add("Arun");
+        list.add("Guna");
+        list.add("Sanjay");
+        list.add("saran prasath");
+        list.add("Dharanish");
         System.out.println();
-        list.remove("Arun");
-        {
+
+
             while (list.size() > 1) {
                 process(list);
             }
-        }
+
     }
 }
