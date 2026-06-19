caf4bf1818b9bba8f17a8050bd4534ae90dfc570
Gokul <gokulr25ct@vetias.ac.in>
2026-06-19 14:13:54 +0530

    Day13.java

---
+++ src/Day_11.java
@@ -0,0 +1,13 @@
+impoet java.util.Hashmap;
+public class Day_11 {
+    public static void main(){
+        Map<String, object> person = new HashMap<>();
+        person.put("name","Gokul");
+        person.put("age", 17);
+        person.put("isYoung", "True");
+        person.put("percent", 89.566);
+
+        System.out.println(person.get("percent"))
+    }
+
+}
