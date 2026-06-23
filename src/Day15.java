172ad4a9c464ebb0dbfb3e9acc616aed8be31817
Gokul <gokulr25ct@vetias.ac.in>
2026-06-23 11:25:18 +0530

    Day15chef.java

---
+++ src/Day15.java
@@ -0,0 +1,20 @@
+public class day15 {
+    static void main() throws InterruptedException {
+        Runnable chefTask = () -> {
+            String[] items = {"parotta", "Briyani", "waffle"};
+            for (int i = 0; i < items.length; i++) {
+                System.out.println("Cooking Item:" + items[i]);
+                try {
+                    Thread.sleep(8000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        };
+        Thread chefThread = new Thread(chefTask, "chef_thread");
+        chefThread.start();
+        chefThread.join();
+
+    }
+}
